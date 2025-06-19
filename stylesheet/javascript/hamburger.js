97d15e389944ca8ac63201ef276c4b86605798f8
Target <TargetCreate923@gmail.com>
2025-06-19 17:00:04 +0900

    もう少し勉強したい

--- stylesheet/javascript/hamburger.js
+++ stylesheet/javascript/hamburger.js
@@ -4,4 +4,11 @@ const mainNavigation = document.getElementById('mainNav');
 hamburgerButton.addEventListener('click', () => {
     mainNavigation.classList.toggle('active');
     hamburgerButton.classList.toggle('open');
+
+    if (mainNavigation.classList.contains('active')) {
+        const navHeight = mainNavigation.offsetHeight; // 要素の高さ
+        hamburgerButton.style.transform = `translateY(${navHeight}px)`;
+    } else {
+        hamburgerButton.style.transform = `translateY(0)`
+    }
 });
